01d4a24fd88518c9d90cd6d73bb6d82e464d5a93
Guilherme Ruiz <guiiruiz@hotmail.com>
2017-07-06 06:24:22 -0300

    Implement Header component

---
+++ app/views/main/elements/header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {
+  View,
+  StyleSheet,
+  Text,
+  Button,
+} from 'react-native';
+
+import { Actions } from 'react-native-router-flux';
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#d15519',
+    paddingTop: 13,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  brand: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
+
+export default class Header extends React.Component {
+
+  handleOnPress(){
+    //TODO: Navigate to Add scene
+  }
+
+  render(){
+    return (
+      <View style={styles.header}>
+        <Text style={styles.brand}>IIDO</Text>
+        <Button color="white" title="Add" onPress={this.handleOnPress}/>
+      </View>
+    )
+  }
+}
